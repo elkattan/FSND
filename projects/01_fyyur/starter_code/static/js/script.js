5a810f0c9b2f78eb44be2bbca29cf575e441b2c9
Ahmed Elkattan <ahmedamrelkattan@gmail.com>
2020-11-21 17:39:00 +0200

    CRUD functions ready

--- projects/01_fyyur/starter_code/static/js/script.js
+++ projects/01_fyyur/starter_code/static/js/script.js
@@ -2,3 +2,9 @@ window.parseISOString = function parseISOString(s) {
   var b = s.split(/\D+/);
   return new Date(Date.UTC(b[0], --b[1], b[2], b[3], b[4], b[5], b[6]));
 };
+
+async function delete_venue(venue) {
+  // Delete request cannot be submited through web forms or anchor tags
+  await fetch("/venues/" + venue, { method: "delete" });
+  window.location.replace("/");
+}
